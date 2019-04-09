1c9a7c833938f139291b82cb90b1d0bc26154845
zhenzhencai <474760615@qq.com>
2019-04-09 16:39:08 +0800

    add js offer

---
+++ jsOffer.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <br>
+  <img width="150" src="./LeetCode.png" alt="logo">
+  <br>
+  <br>
+</p>
+
+## 剑指offer
+剑指offer JavaScript版本
+
+---
+
+| 编号 | 题目 | 代码 | 说明 |
+|:---:|:---:|:---:|:---:|
+| 面试题3 | 数组中的重复数字 | [code](https://github.com/zhenzhencai/leetcode/blob/master/jsOffer/003.js) | 两道题目 |
+
---
+++ jsOffer/003.js
@@ -0,0 +1,40 @@
+/*
+ * @Author: Zhenzhen Cai 
+ * @Date: 2019-04-09 16:01:25 
+ * @Last Modified by: Zhenzhen Cai
+ * @Last Modified time: 2019-04-09 16:12:44
+ */
+
+
+/**
+ *给定一个长度为 n 的整数数组 nums，数组中所有的数字都在 0∼n−1 的范围内。
+ *数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。
+ *请找出数组中任意一个重复的数字。
+ *
+ * @param {*} arr
+ */
+function swap(arr,i,j){
+    let temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+
+function duplicate(arr){
+    let res=[];
+    for(let i=0;i<arr.length;i++){
+        let val=arr[i];
+        while(val!=i){
+            if(val===arr[val]){
+                res.push(val);
+                break;
+            }
+            swap(arr,i,val);
+        }
+    }
+    return res;
+}
+
+let arr=[3,1,2,0,2,5,3];
+let res=duplicate(arr);
+console.log(res);
+
